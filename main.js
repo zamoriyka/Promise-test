568551edca86cc652b02e3b2680031d5c772cfd8
Anna Zamoriy <zamoriy,ania@gmail,com>
2019-03-09 16:17:12 +0200

    image loaded feature

--- main.js
+++ main.js
@@ -20,3 +20,22 @@
 //     .catch(error => {
 //         console.log("Request faild", error)
 //     })
+
+const url1 = "https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/549585-istock-909106260.jpg?itok=ds7LqH1N&resize=1100x1100";
+const url2 = "https://i.ytimg.com/vi/InkWIPUFVdM/maxresdefault.jpg";
+const url3 = "https://cdn.britannica.com/67/197567-131-1645A26E.jpg";
+
+function loadImages(url) {
+    return new Promise((resole) => {
+        const img = new Image();
+        document.body.append(img);
+        img.addEventListener("load", () => {
+            resolve();
+        })
+    })
+}
+
+loadImages(url1)
+    .then(() => loadImages(url2), console.log('First image loaded'))
+    .then(() => loadImages(url3), console.log('Second image loaded'))
+console.log("Third image loaded")
